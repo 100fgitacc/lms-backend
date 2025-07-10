7e672fd8a8828765af97dee64dc06069c5d40672
100fgitacc <100fsiteblog@gmail.com>
2025-07-10 12:28:15 +0300

    recaptcha add

--- controllers/auth.js
+++ controllers/auth.js
@@ -161,79 +161,82 @@ exports.signup = async (req, res) => {
 
 
 // ================ LOGIN ================
+
 exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password, recaptcha } = req.body;
 
-        // validation
-        if (!email || !password) {
-            return res.status(400).json({
-                success: false,
-                message: 'All fields are required'
-            });
-        }
+    const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+    const verifyURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptcha}`;
 
-        // check user is registered and saved data in DB
-        let user = await User.findOne({ email }).populate('additionalDetails');
+    const captchaRes = await fetch(verifyURL, { method: "POST" });
+    const captchaData = await captchaRes.json();
 
-        if (!user) {
-            return res.status(401).json({
-                success: false,
-                message: 'You are not registered with us'
-            });
-        }
-
-
-        // comapare given password and saved password from DB
-        if (await bcrypt.compare(password, user.password)) {
-            const payload = {
-                email: user.email,
-                id: user._id,
-                accountType: user.accountType // This will help to check whether user have access to route, while authorzation
-            };
-
-            // Generate token 
-            const token = jwt.sign(payload, process.env.JWT_SECRET, {
-                expiresIn: "24h",
-            });
-
-            user = user.toObject();
-            user.token = token;
-            user.password = undefined; // we have remove password from object, not DB
-
-
-            // cookie
-            const cookieOptions = {
-                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // 3 days
-                httpOnly: true
-            }
-
-            res.cookie('token', token, cookieOptions).status(200).json({
-                success: true,
-                user,
-                token,
-                message: 'User logged in successfully'
-            });
-        }
-        // password not match
-        else {
-            return res.status(401).json({
-                success: false,
-                message: 'Password not matched'
-            });
-        }
+    if (!captchaData.success || captchaData.score < 0.5) {
+      return res.status(403).json({
+        success: false,
+        message: "reCAPTCHA verification failed. Possibly a bot.",
+      });
+    }
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
 
-    catch (error) {
-        console.log('Error while Login user');
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            error: error.message,
-            message: 'Error while Login user'
-        })
+    let user = await User.findOne({ email }).populate("additionalDetails");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "You are not registered with us",
+      });
     }
-}
+
+    if (await bcrypt.compare(password, user.password)) {
+      const payload = {
+        email: user.email,
+        id: user._id,
+        accountType: user.accountType,
+      };
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "24h",
+      });
+
+      user = user.toObject();
+      user.token = token;
+      user.password = undefined;
+
+      const cookieOptions = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+      res.cookie("token", token, cookieOptions).status(200).json({
+        success: true,
+        user,
+        token,
+        message: "User logged in successfully",
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Password not matched",
+      });
+    }
+  } catch (error) {
+    console.log("Error while Login user");
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: error.message,
+      message: "Error while Login user",
+    });
+  }
+};
+
 
 
 // ================ CHANGE PASSWORD ================
